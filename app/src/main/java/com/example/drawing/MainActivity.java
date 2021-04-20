1cc10008d59c139582c095aee22a95b1c14fd925
Bakyt <djumabaevb@gmail.com>
2021-04-20 14:13:38 +0600

    implicit intent

--- app/src/main/java/com/example/drawing/MainActivity.java
+++ app/src/main/java/com/example/drawing/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.drawing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, "This is the message from implicit intent!");
+
+        });
+
     }
 }
